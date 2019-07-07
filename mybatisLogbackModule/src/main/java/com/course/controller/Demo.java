ad68f746125f482218b94453f3b2d4a90b1cdd67
mars-fung <530263348@qq.com>
2019-07-08 00:16:51 +0800

    mybatis

--- mybatisLogbackModule/src/main/java/com/course/controller/Demo.java
+++ mybatisLogbackModule/src/main/java/com/course/controller/Demo.java
@@ -1,13 +1,12 @@
 package com.course.controller;
 
+import com.course.module.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Administrator on 2019/7/4.
@@ -29,7 +28,27 @@ public class Demo {
     public int getUserCount(){
         //这个（）里的是输入mapper文件里的id
         int result = template.selectOne("getUserCount");
+        return result;
+    }
 
+    @ApiOperation(value = "插入用户",httpMethod = "POST")
+    @PostMapping("/addUser")
+    public int addUser(@RequestBody User user){
+        int result =  template.insert("addUser",user);
+        return  result;
+    }
+
+    @ApiOperation(value = "更新用户",httpMethod = "POST")
+    @PostMapping("updateUser")
+    public int updateUser(@RequestBody User user){
+        int result = template.update("updateUser",user);
+        return result;
+    }
+
+    @ApiOperation(value = "删除用户",httpMethod = "POST")
+    @PostMapping("deleteUser")
+    public int deleteUser(@RequestParam Integer id){
+        int result = template.delete("deleteUser",id);
         return result;
     }
 
---
+++ mybatisLogbackModule/src/main/java/com/course/module/User.java
@@ -0,0 +1,17 @@
+package com.course.module;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2019/7/7.
+ */
+
+@Data
+public class User {
+        private int id;
+        private String name;
+        private int age;
+        private String sex;
+
+
+}
